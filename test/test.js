ebababc5f59e79acc21d06d0f91e1aa518ed5e69
Giacomo Rossetto <jackyman_cs4@live.it>
2018-04-13 16:55:22 +0200

    style(tests): lint with standardjs

--- test/test.js
+++ test/test.js
@@ -1,30 +1,30 @@
-'use strict';
-var chai = require('chai');
-var expect = chai.expect;
-var isGitlabUrl = require('../index.js');
-var options;
+'use strict'
+var chai = require('chai')
+var expect = chai.expect
+var isGitlabUrl = require('../index.js')
+var options
 
 var userUrls = [
   'https://gitlab.com/facebook',
   'https://gitlab.com/facebook/'
-];
+]
 
 var plainGhURLs = [
   'https://gitlab.com/'
-];
+]
 
 var repoUrls = [
   'https://gitlab.com/facebook/react',
   'https://gitlab.com/facebook/react',
   'https://gitlab.com/facebook/_re.act/'
-];
+]
 
 var expandedUrls = [
   'https://gitlab.com/facebook/react/tree/0.14-stable',
   'https://gitlab.com/facebook/react/releases/tag/v0.14.0',
   'https://gitlab.com/alferov/awesome-gulp/blob/master/.gitignore#L2',
   'https://gitlab.com/alferov/is-gitlab-url/pulls?q=is%3Apr+is%3Aclosed'
-];
+]
 
 var cloningUrls = [
   'git://gitlab.com/facebook/react.git#gh-pages',
@@ -33,7 +33,7 @@ var cloningUrls = [
   'https://gitlab.com/facebook/react.git',
   // 'https://gitlab.com/facebook/react',
   'git://gitlab.com:user/some_project.git.git'
-];
+]
 
 var invalidUrls = [
   'gitlab.com/',
@@ -54,134 +54,128 @@ var invalidUrls = [
   'https://gitlab.com/face_book',
   'https://gitlab.com:alferov/awesome-gulp#gh-pages/',
   'https://gitlab.com/facebook/react/.git'
-];
+]
 
-describe('is-gitlab-url', function() {
-  describe('with a standard set of options', function() {
+describe('is-gitlab-url', function () {
+  describe('with a standard set of options', function () {
+    plainGhURLs.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url)).to.be.true
+      })
+    })
 
-    plainGhURLs.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url)).to.be.true;
-      });
-    });
+    repoUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url)).to.be.true
+      })
+    })
 
-    repoUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url)).to.be.true;
-      });
-    });
+    expandedUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url)).to.be.true
+      })
+    })
 
-    expandedUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url)).to.be.true;
-      });
-    });
+    userUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url)).to.be.true
+      })
+    })
 
-    userUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url)).to.be.true;
-      });
-    });
+    cloningUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url)).to.be.true
+      })
+    })
 
-    cloningUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url)).to.be.true;
-      });
-    });
+    invalidUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url)).to.be.false
+      })
+    })
+  })
 
-    invalidUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url)).to.be.false;
-      });
-    });
+  describe('with enabled strict mode', function () {
+    before(function () {
+      options = { strict: true }
+    })
 
-  });
+    plainGhURLs.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-  describe('with enabled strict mode', function() {
+    repoUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-    before(function() {
-      options = { strict: true };
-    });
+    expandedUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-    plainGhURLs.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
+    userUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-    repoUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
+    cloningUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url, options)).to.be.true
+      })
+    })
 
-    expandedUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
+    invalidUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
+  })
 
-    userUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
+  describe('with enabled repository mode', function () {
+    before(function () {
+      options = { repository: true }
+    })
 
-    cloningUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url, options)).to.be.true;
-      });
-    });
+    plainGhURLs.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-    invalidUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
+    repoUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url, options)).to.be.true
+      })
+    })
 
-  });
+    expandedUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-  describe('with enabled repository mode', function() {
+    userUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
 
-    before(function() {
-      options = { repository: true };
-    });
+    cloningUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be valid', function () {
+        expect(isGitlabUrl(url, options)).to.be.true
+      })
+    })
 
-    plainGhURLs.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
-
-    repoUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url, options)).to.be.true;
-      });
-    });
-
-    expandedUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
-
-    userUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
-
-    cloningUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be valid', function() {
-        expect(isGitlabUrl(url, options)).to.be.true;
-      });
-    });
-
-    invalidUrls.forEach(function(url) {
-      it('URL' + ' - ' + url + ' should be invalid', function() {
-        expect(isGitlabUrl(url, options)).to.be.false;
-      });
-    });
-
-  });
-});
+    invalidUrls.forEach(function (url) {
+      it('URL' + ' - ' + url + ' should be invalid', function () {
+        expect(isGitlabUrl(url, options)).to.be.false
+      })
+    })
+  })
+})
